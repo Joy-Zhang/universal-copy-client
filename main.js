2a479857456bb7036407ebaef1630bbc148e3c78
Joy Zhang <twoleft.zhang@outlook.com>
2020-10-13 18:09:34 +0800

    基本同步功能

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, Menu, Tray, clipboard } = require('electron');
+const axios = require('axios');
+const { version } = require('./package.json')
+const WebSocket = require('ws');
+
+const SERVER = `localhost:8100/${version}`;
+
+const instance = axios.create({
+    baseURL: `http://${SERVER}`,
+    timeout: 3000
+});
+
+let lastContent = null;
+
+const ws = new WebSocket(`ws://${SERVER}/events`);
+console.log(ws.on)
+
+ws.addListener('message', (data) => {
+    const message = JSON.parse(data);
+    console.log(message);
+    if (message.event === 'copy') {
+        if (message.content) {
+            lastContent = message.content;
+            clipboard.writeText(lastContent);
+        }
+    }
+});
+
+(function () {
+    setTimeout(function poll() {
+        const content = clipboard.readText();
+        if (content !== null && lastContent !== content) {
+            lastContent = content;
+            instance.post('/copy', JSON.stringify({content: lastContent}));
+        }
+        setTimeout(poll, 500);
+    }, 0);
+})();
+
+
+let tray = null;
+app.on('ready', () => {
+    tray = new Tray('./clipboard_white.png');
+    instance.get('/clipboard').then((response) => {
+        console.log(response);
+        const template = response.data.clipboard.map(function (item) {
+            return {label: item, type: 'normal'}
+        });
+        const contextMenu = Menu.buildFromTemplate(template.length > 0 ? template : [
+            { label: 'Empty', type: 'normal' }
+        ]);
+        tray.setContextMenu(contextMenu);
+    }).catch((exception) => {
+        console.log(exception);
+    });
+});
+
+
+app.on('window-all-closed', () => {
+    app.quit()
+});
